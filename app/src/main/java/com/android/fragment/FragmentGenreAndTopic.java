bf52765213ff4394092de0cb93a9954d2414b8b8
An Nguyen <nnan22618@gmail.com>
2019-12-12 01:01:01 +0700

    push
    Test

---
+++ app/src/main/java/com/android/fragment/FragmentGenreAndTopic.java
@@ -0,0 +1,101 @@
+package com.android.fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import com.android.mainapp.R;
+import com.android.model.ChuDe;
+import com.android.model.ChuDeVaTheLoaiTrongNgay;
+import com.android.model.TheLoai;
+import com.android.service.APIService;
+import com.android.service.Dataservice;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FragmentGenreAndTopic extends Fragment {
+
+    View view;
+    HorizontalScrollView horizontalScrollView;
+    TextView txtseemore;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_genre_topic, container,false);
+        horizontalScrollView =  view.findViewById(R.id.horizontalScrollview);
+        txtseemore = view.findViewById(R.id.textviewseemore);
+        GetData();
+        return view;
+    }
+
+    private void GetData() {
+        Dataservice dataservice = APIService.getService();
+        Call<ChuDeVaTheLoaiTrongNgay> callback = dataservice.GetGenreAndTopic();
+        callback.enqueue(new Callback<ChuDeVaTheLoaiTrongNgay>() {
+            @Override
+            public void onResponse(Call<ChuDeVaTheLoaiTrongNgay> call, Response<ChuDeVaTheLoaiTrongNgay> response) {
+                ChuDeVaTheLoaiTrongNgay genreandtopic = response.body();
+
+                final ArrayList<ChuDe> topicArrayList = new ArrayList<>();
+                topicArrayList.addAll(genreandtopic.getChuDe());
+
+                final ArrayList<TheLoai> genreArrayList = new ArrayList<>();
+                genreArrayList.addAll(genreandtopic.getTheLoai());
+
+
+                LinearLayout linearLayout = new LinearLayout(getActivity());
+                linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+                LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(580,550);
+                layout.setMargins(10,20,10,30);
+                for(int i = 0 ; i < (topicArrayList.size()); i++){
+                    CardView cardView = new CardView(getActivity());
+                    cardView.setRadius(10);
+                    ImageView imageView = new ImageView(getActivity());
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    if(topicArrayList.get(i).getTopicThumb() != null){
+                        Picasso.with(getActivity()).load(topicArrayList.get(i).getTopicThumb()).into(imageView);
+                    }
+                    cardView.setLayoutParams(layout);
+                    cardView.addView(imageView);
+                    linearLayout.addView(cardView);
+                }
+                for(int j = 0 ; j < (genreArrayList.size()); j++){
+                    CardView cardView = new CardView(getActivity());
+                    cardView.setRadius(10);
+                    ImageView imageView = new ImageView(getActivity());
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    if(genreArrayList.get(j).getGenreThumb() != null){
+                        Picasso.with(getActivity()).load(genreArrayList.get(j).getGenreThumb()).into(imageView);
+                    }
+                    cardView.setLayoutParams(layout);
+                    cardView.addView(imageView);
+                    linearLayout.addView(cardView);
+                }
+                horizontalScrollView.addView(linearLayout);
+            }
+
+            @Override
+            public void onFailure(Call<ChuDeVaTheLoaiTrongNgay> call, Throwable t) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/android/model/ChuDe.java
@@ -0,0 +1,42 @@
+package com.android.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ChuDe {
+
+@SerializedName("Topic_Id")
+@Expose
+private String topicId;
+@SerializedName("Topic_Name")
+@Expose
+private String topicName;
+@SerializedName("Topic_Thumb")
+@Expose
+private String topicThumb;
+
+public String getTopicId() {
+return topicId;
+}
+
+public void setTopicId(String topicId) {
+this.topicId = topicId;
+}
+
+public String getTopicName() {
+return topicName;
+}
+
+public void setTopicName(String topicName) {
+this.topicName = topicName;
+}
+
+public String getTopicThumb() {
+return topicThumb;
+}
+
+public void setTopicThumb(String topicThumb) {
+this.topicThumb = topicThumb;
+}
+
+}
